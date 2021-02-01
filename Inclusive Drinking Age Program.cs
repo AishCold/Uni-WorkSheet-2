8e7bdf39fced50bde1f6616ed11f5d2947c461d9
AishCold <72552823+AishCold@users.noreply.github.com>
2021-02-01 04:46:57 +0000

    Add files via upload

---
+++ Inclusive Drinking Age Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Please enter your height in cm.");
+
+		//user inputs height
+		//user's input is stored in string variable 'userHeight'
+
+		string userHeight = Console.ReadLine();
+
+		//userHeight is converted to float
+
+		float height;
+		height = float.Parse(userHeight);
+
+		//Checking that the user's height meets the requirments
+
+		if(height >= 120 )
+        {
+			Console.WriteLine("You are tall enough to ride the rollercoaster");
+        }
+        else
+        {
+			Console.WriteLine("Sorry, you are not tall enough to ride the rollercoaster");
+		}
+
+		
+
+	}
+}
---
+++ Theme Park Conditional Program 2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    //In this program, the user has to guess the random number (1-10)
+    static void Main()
+    {
+        Console.WriteLine("Simple Guessing Game");
+
+        Random random = new Random();
+
+        int secretNumber = random.Next(1, 11);
+
+        Console.WriteLine("Guess the random number!");
+        string inputString = Console.ReadLine();
+
+        int userGuess = int.Parse(inputString);
+
+        if (secretNumber == userGuess)
+        {
+            Console.WriteLine("Congratulations, you win!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you lose!");
+        }
+    }
+}
+
